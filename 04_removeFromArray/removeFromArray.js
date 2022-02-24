82d6c9ef1af2e969303598f17d7f3e83c49ff2de
Oussama <ousstarhi.web@gmail.com>
2022-02-24 21:24:59 +0100

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray1 = function (arr, ...args) {
+    const arrLength = arr.length;
+    const returnedArr = [];
+    for (let i = 0; i < arrLength; i++) {
+        if (args.indexOf(arr[i]) === -1) {
+            returnedArr.push(arr[i]);
+        }
+    }
+    return returnedArr;
 };
-
+const removeFromArray = function (arr, ...args) {
+    const argsLength = args.length;
+    for (let i = 0; i < argsLength; i++) {
+        const index = arr.indexOf(args[i]);
+        if (index !== -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
