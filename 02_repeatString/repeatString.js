77eb4e5df3d8af5b68d7c3481f03f7f1bf112f78
Oussama <ousstarhi.web@gmail.com>
2022-02-24 20:59:15 +0100

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let message = '';
+    for (let i = 0; i < n; i++) {
+        message += str;
+    }
+    return message;
 };
 
 // Do not edit below this line
